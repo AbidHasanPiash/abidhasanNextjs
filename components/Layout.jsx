4fd7f113448e5237ee7550622e8a766cd4f2107c
Abid <meghpiash@gmail.com>
2023-04-04 03:18:03 +0600

    re upload

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,16 @@
 import Navbar from "./Navbar"
 import NavbarM from "./NavbarM"
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
+  const location = useRouter();
   return (
     <>
-      <Navbar />
+      {location.asPath != '/bankofabid' && <Navbar />}
       <main>
         {children}
       </main>
-      <NavbarM/>
+      {location.asPath != '/bankofabid' && <NavbarM/>}
     </>
   )
 }
