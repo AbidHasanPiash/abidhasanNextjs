6038185969d3fbf361d7e6658e2c0f60e427348a
Abid <meghpiash@gmail.com>
2023-04-30 14:22:22 +0600

    polished

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,8 +25,8 @@ export default function Home() {
           <div className="lg:h-4/6 grid lg:grid-cols-2 ">
             {/* Text and Button */}
             <div className="h-fit lg:h-full flex flex-col items-start lg:justify-center mx-3 space-y-10">
-              <h1 className="lg:text-6xl text-3xl lg:font-extrabold">
-                The Web Designer That You Were Waiting For,
+              <h1 className="lg:text-6xl text-3xl font-extrabold text-center lg:text-start">
+                The Web Designer That You Were Looking For,
               </h1>
               <div className="flex w-full items-center justify-center lg:justify-start lg:items-start space-x-6 scale-90 lg:scale-100">
                 <BtnTwoSideGlow text="Get Started." link="/about"/>
@@ -38,7 +38,7 @@ export default function Home() {
               {/* Card Back part */}
               <div
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-6
-                w-52 h-60 lg:w-72 lg:h-80 rounded-xl overflow-hidden duration-200
+                w-60 h-72 lg:w-72 lg:h-80 rounded-xl overflow-hidden duration-200
                 group-hover:-translate-x-[10%] group-hover:-translate-y-[50%] group-hover:rotate-2"
               >
                 <div className="w-full h-full relative p-6 bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900">
@@ -47,33 +47,33 @@ export default function Home() {
                   <div className="w-10 h-10 border-b-2 border-r-2 border-white/20 absolute bottom-4 right-4" />
                   <div className="w-10 h-10 border-b-2 border-l-2 border-white/20 absolute bottom-4 left-4" />
                   <div className="h-full flex flex-col items-center justify-center">
-                    <p className="text-xl">Abid Hasan</p>
-                    <p className="text-sm">B.Sc.(Engg.) in CSE</p>
+                    <p className="text-2xl">Abid Hasan</p>
+                    <p className="text-lg">B.Sc.(Engg.) in CSE</p>
                   </div>
                 </div>
               </div>
               {/* Card Front part */}
               <div
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-3 
-                w-52 h-60 lg:w-72 lg:h-80 rounded-xl overflow-hidden duration-300
+                w-60 h-72 lg:w-72 lg:h-80 rounded-xl overflow-hidden duration-300
                 group-hover:-translate-x-[90%] group-hover:-translate-y-[40%] group-hover:-rotate-6"
               >
                 <div className="w-full h-full relative flex items-end bg-gradient-to-r from-violet-200 to-pink-200">
-                  <div className="lg:w-60 lg:h-60 w-40 h-40 absolute bottom-5 left-6 bg-gradient-to-r from-fuchsia-500 to-purple-500 rounded-full shadow-5xl" />
+                  <div className="lg:w-60 lg:h-60 w-48 h-48 absolute bottom-5 left-6 bg-gradient-to-r from-fuchsia-500 to-purple-500 rounded-full shadow-5xl" />
                   <Image src={profile} alt="profile" className="z-10" />
                 </div>
               </div>
               {/* Stamp */}
               <div 
                 className="w-fit h-fit absolute left-10 top-5 lg:left-32 lg:top-24 scale-75 lg:scale-100
-                rounded-full bg-white/60 backdrop-blur-sm shadow-neon"
+                rounded-full bg-gray-300/60 backdrop-blur-sm "
               >
                 <Stamp text="Creative Developer." />
               </div>
             </div>
           </div>
           {/* hero - down section */}
-          <div className="lg:h-2/6 flex flex-col-reverse lg:flex-row items-center justify-between px-3 ">
+          <div className="lg:h-2/6 flex flex-col-reverse lg:flex-row items-center justify-between px-3 pb-14">
             <div className="h-40 flex items-center justify-center">
               <Player autoplay loop src={programmingComputerJson}/>
             </div>
