d774ac73e6a5067476416c5538d7298f024fff94
Abid <meghpiash@gmail.com>
2023-05-02 14:16:07 +0600

    new update

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,7 @@ import profile from "../public//image/profile.png";
 import { Player } from '@lottiefiles/react-lottie-player';
 import programmingComputerJson from "../public//json/programming-computer.json";
 import GlowingPyramid from "@/components/common/GlowingPyramid";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
@@ -18,7 +19,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className="lg:max-w-7xl h-screen lg:mx-auto mt-20 px-3">
         {/* hero */}
         <div className="h-screen">
           {/* hero - upper section */}
@@ -91,7 +92,7 @@ export default function Home() {
           <div className="grid lg:grid-cols-3 gap-5 mx-auto text-gray-900">
             {/* services 1..................................... */}
             <div className="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-indigo-200 via-purple-900 to-indigo-200 rounded-xl p-5 relative">
-              <h1 className="text-2xl font-bold">UI Design:</h1> 
+              <h1 className="text-2xl font-bold">UI/UX Design:</h1> 
               <BiPaintRoll className="absolute right-2 bottom-2 text-9xl text-[#6a5591]/70"/>
               <p className="text-lg text-justify pt-5">
                 Creating the visual and interactive elements that users see 
@@ -100,7 +101,7 @@ export default function Home() {
             </div>
             {/* services 2..................................... */}
             <div className="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-200 via-red-500 to-fuchsia-500 rounded-xl p-5 relative">
-              <h1 className="text-2xl font-bold">Implementing the design:</h1>
+              <h1 className="text-2xl font-bold">Develop Web App:</h1>
               <BiCodeAlt className="absolute right-2 bottom-2 text-9xl text-[#cc6f3d]/70"/>
               <p className="text-lg text-justify pt-5">
                 Using <span className="font-bold">React</span> to bring the design to life, ensuring it is responsive 
@@ -110,19 +111,17 @@ export default function Home() {
             </div>
             {/* services 3..................................... */}
             <div className="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200 rounded-xl p-5 relative">
-              <h1 className="text-2xl font-bold">Implementing the design:</h1>
+              <h1 className="text-2xl font-bold">Update Existing:</h1>
               <BiGitBranch className="absolute right-2 bottom-2 text-9xl text-[#556791]/70"/>
               <p className="text-lg text-justify pt-5">
-                Using <span className="font-bold">React</span> to bring the design to life, ensuring it is responsive 
-                and collaborating with backend developers to integrate the 
-                frontend with the backend functionality.
+                Modefy or Redesign your existing project for your requirments. I will be happy to contribute with you.
               </p>
             </div>
           </div>
         </div>
         <div className="grid lg:grid-cols-2 py-10 px-3 ">
           <div className="col-span-1 flex flex-col items-start justify-center">
-            <h1 className="text-7xl font-extralight ">What I Do.</h1>
+            <h1 className="text-7xl font-extralight ">What I Can.</h1>
             <ul className="pt-5 lg:pl-10 text-lg space-y-4">
               <li className="flex items-center justify-start space-x-3" ><span><ImCheckmark2/></span> <span>Designing user interfaces (UI)</span></li>
               <li className="flex items-center justify-start space-x-3" ><span><ImCheckmark2/></span> <span>Developing responsive and user-friendly websites.</span></li>
@@ -136,9 +135,7 @@ export default function Home() {
             <GlowingPyramid/>
           </div>
         </div>
-        <div className="h-96 flex items-end justify-center ">
-          footer
-        </div>
+        <Footer/>
       </div>
     </>
   );
--- pages/works.jsx
+++ pages/works.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Tiltcard from "@/components/portfolio/Tiltcard";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { SwiperComponent } from "@/components/portfolio/Swiper";
 
 export default function Works() {
@@ -11,50 +12,53 @@ export default function Works() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <Parallax pages={2}>
+      <div className="lg:max-w-7xl h-screen lg:mx-auto mt-16 px-3">
         {/* ============Webapps=========== */}
+        <ParallaxLayer speed={0.7} factor={0.5}  style={{ backgroundColor: '#0f172a' }} className="z-10">
         <div className="lg:h-fit lg:max-w-7xl lg:mx-auto mx-3 my-20 bg-[#0f172a]">
           <div className="relative h-full text-white/80">
-            <div className="absolute z-0 lg:w-32 lg:h-32 w-36 h-36 animate-spin-slow top-1/4 lg:left-28 left-0 bg-opacity-20 bg-gradient-to-r from-blue-900 to-purple-700 rounded-full"/>
-            <div className="absolute z-0 lg:w-60 lg:h-60 w-44 h-44 animate-spin-slow bottom-0 right-1/2 bg-opacity-20 bg-gradient-to-r from-rose-500 to-purple-500 rounded-full"/>
-            <div className="absolute z-0 lg:w-40 lg:h-40 w-36 h-36 animate-spin-slow top-1/3 lg:right-20 right-0 bg-opacity-20 bg-gradient-to-r from-emerald-700 to-cyan-500 rounded-full"/>
             <h1 className="text-2xl text-center font-bold my-10">
               Some of my Web Applications.
             </h1>
+            <div className="absolute z-0 lg:w-32 lg:h-32 w-36 h-36 animate-spin-slow top-1/4 lg:left-28 left-0 bg-opacity-20 bg-gradient-to-r from-blue-900 to-purple-700 rounded-full"/>
+            <div className="absolute z-0 lg:w-60 lg:h-60 w-44 h-44 animate-spin-slow bottom-0 right-1/2 bg-opacity-20 bg-gradient-to-r from-rose-500 to-purple-500 rounded-full"/>
+            <div className="absolute z-0 lg:w-40 lg:h-40 w-36 h-36 animate-spin-slow top-1/3 lg:right-20 right-0 bg-opacity-20 bg-gradient-to-r from-emerald-700 to-cyan-500 rounded-full"/>
             <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-3">
-              <Tiltcard />
-              <Tiltcard />
+              <Tiltcard/>
+              <Tiltcard/>
             </div>
           </div>
         </div>
+        </ParallaxLayer>
         {/* ============Extra text=========== */}
+        <ParallaxLayer offset={0.7} speed={-0.5} factor={0.5} className="z-0">
         <div className="lg:max-w-7xl lg:mx-auto mx-3">
           <div className="flex flex-col items-center justify-center">
             <div className="text-center">
-              <h1 className="lg:text-4xl text-lg font-bold">
-                &quot;<span className="lg:text-7xl text-2xl">Art</span> is not
-                what you see,
-                <br />
-                but what you <span className="lg:text-7xl text-2xl">
-                  make
-                </span>{" "}
-                others see.&quot;
-              </h1>
-              <p className="text-right mt-4">- Edgar Degas</p>
+              <h1 className="lg:text-4xl text-2xl font-extralight">&quot;Good <span className="lg:text-7xl text-4xl">design</span>  is obvious.
+                <br />Great design is <span className="lg:text-7xl text-4xl">transparent.</span>&quot;</h1>
+              <p className="text-right mt-4">- Joe Sparano</p>
+              <ParallaxLayer speed={1}>
               <div className="absolute top-1/2 inset-x-1/2 z-0 lg:w-40 lg:h-40 w-36 h-36 animate-spin-slow bg-opacity-20 bg-gradient-to-r from-emerald-700 to-cyan-500 rounded-full"></div>
+              </ParallaxLayer>
             </div>
           </div>
         </div>
+        </ParallaxLayer>
         {/* ============Website=========== */}
+        <ParallaxLayer offset={1} factor={1.5} speed={1.5} style={{ backgroundColor: '#0f172a' }}>
         <div className="lg:max-w-7xl lg:mx-auto mx-3 my-20">
           <h1 className="text-2xl text-center font-bold my-10">
             Showcasing My Front-End Craftsmanship
           </h1>
           <div>
-            <SwiperComponent />
+            <SwiperComponent/>
           </div>
         </div>
+        </ParallaxLayer>
       </div>
+      </Parallax>
     </>
   );
 }
